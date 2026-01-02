a04aa463dbafe2655365860432e0154b728afef2
Your Name <27amad27@gmail.com>
2026-01-01 19:18:37 -0500

    added gitsigns

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -104,4 +104,21 @@ return {
       end, { desc = 'Select prompt' })
     end,
   },
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup {
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          -- Keymaps
+          vim.keymap.set('n', '<leader>hp', gs.preview_hunk, { buffer = bufnr })
+          vim.keymap.set('n', '<leader>hd', gs.diffthis, { buffer = bufnr })
+          vim.keymap.set('n', '<leader>hb', gs.blame_line, { buffer = bufnr })
+          vim.keymap.set('n', ']c', gs.next_hunk, { buffer = bufnr })
+          vim.keymap.set('n', '[c', gs.prev_hunk, { buffer = bufnr })
+        end,
+      }
+    end,
+  },
 }
